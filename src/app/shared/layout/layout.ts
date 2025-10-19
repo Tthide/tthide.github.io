22a82f2acb4455956ae0646ea4f4e77c84130c10
Tthide <bordethibault@gmail.com>
2025-10-19 17:14:50 +0200

    feat: added skip to content link

--- src/app/shared/layout/layout.ts
+++ src/app/shared/layout/layout.ts
@@ -8,14 +8,19 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [HeaderComponent, FooterComponent, RouterOutlet],
   template: `
-    <div class="flex flex-col bg-light min-h-svh">    
+    <div class="flex flex-col bg-light min-h-svh">
+        <!-- Skip link -->
+      <a href="#main" 
+  class="absolute left-2 -top-20 focus:top-4 bg-accent_purple text-light p-3 rounded-md z-[1000] transition-all duration-200 
+        focus-visible:top-4">
+      Skip to content</a>
       <app-header />
-      <main class=" grow px-6 py-5 lg:py-16 md:px-[7%] lg:px-[10%]">
+      <main id="main" tabindex="-1" class=" grow px-6 py-5 lg:py-16 md:px-[7%] lg:px-[10%]">
         <router-outlet />
       </main>
       <app-footer class="px-0 md:px-[7%] lg:px-[10%]" />
     </div>
   `,
 })
-export class LayoutComponent {}
+export class LayoutComponent { }
 
