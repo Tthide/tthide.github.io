5fddb7e906248cd3bca3adca9ace30ee695d2024
Tthide <bordethibault@gmail.com>
2025-10-25 16:11:12 +0200

    feat: adapted rocket from @eva_trostlos, Codepen

---
+++ src/app/shared/rocket/rocket.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rocket',
+  imports: [],
+  templateUrl: './rocket.html',
+  styleUrl: './rocket.css'
+})
+export class Rocket {
+
+}
