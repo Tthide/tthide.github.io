d18b4bac154b596beea94628f4174b5bd9d55a96
Tthide <bordethibault@gmail.com>
2025-10-25 17:31:50 +0200

    feature: rocket launch animation

--- src/app/shared/rocket/rocket.ts
+++ src/app/shared/rocket/rocket.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild } from '@angular/core';
 @Component({
   selector: 'app-rocket',
   imports: [],
@@ -7,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrl: './rocket.css'
 })
 export class Rocket {
+  @ViewChild('rocket') rocketElement!: ElementRef<HTMLDivElement>;
 
+  launchRocket() {
+    const el = this.rocketElement.nativeElement;
+
+    // remove any existing animation class (for reuse)
+    el.classList.remove('animate-launch');
+
+    // Force reflow so animation restarts if called again
+    void el.offsetWidth;
+
+    // Add the animation class
+    el.classList.add('animate-launch');
+  }
 }
