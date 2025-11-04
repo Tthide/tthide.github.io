10bf310b4abaa85cedc8e9d1ad93cf6bf95ca257
Tthide <bordethibault@gmail.com>
2025-11-04 11:42:33 +0100

    feat: reintroduced shadows chip list

--- src/app/project-list/project-list.ts
+++ src/app/project-list/project-list.ts
@@ -1,7 +1,7 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, NgZone } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DataService, Project } from '../services/data.service';
-import { Swiper } from 'swiper';
+import Swiper from 'swiper';
 import { FreeMode } from 'swiper/modules';
 
 @Component({
@@ -16,9 +16,14 @@ import { FreeMode } from 'swiper/modules';
 })
 export class ProjectListComponent implements AfterViewInit, OnInit {
   projects: Project[] = [];
-
-  constructor(private dataService: DataService) { }
-
+  showLeftShadow: { [key: number]: boolean } = {};
+  showRightShadow: { [key: number]: boolean } = {};
+  
+  constructor(
+    private dataService: DataService,
+    private ngZone: NgZone
+  ) { }
+  
   chipColors = [
     'bg-purple-600',
     'bg-blue-500',
@@ -26,38 +31,63 @@ export class ProjectListComponent implements AfterViewInit, OnInit {
     'bg-yellow-500',
     'bg-pink-500'
   ];
-
+  
   getChipColor(index: number): string {
-    // Rotate colors if more keywords than colors
-    //console.log(index);
     return this.chipColors[index % this.chipColors.length];
   }
-
-
+  
   ngAfterViewInit() {
-    setTimeout(() => {
-      const chipSwipers = document.querySelectorAll('.chips-swiper');
-      chipSwipers.forEach((el) => {
-        new Swiper(el as HTMLElement, {
-          modules: [FreeMode], 
-          slidesPerView: 'auto',
-          centeredSlides: false,
-          spaceBetween: 12, 
-          grabCursor: true,
-          freeMode: {
-            enabled: true,
-            sticky: false,
-          },
-          loop: false,
+    // Run Swiper initialization outside Angular zone to avoid interference
+    this.ngZone.runOutsideAngular(() => {
+      setTimeout(() => {
+        const chipSwipers = document.querySelectorAll('.chips-swiper');
+        chipSwipers.forEach((el, index) => {
+          const swiper = new Swiper(el as HTMLElement, {
+            modules: [FreeMode], 
+            slidesPerView: 'auto',
+            centeredSlides: false,
+            spaceBetween: 12, 
+            grabCursor: true,
+            freeMode: {
+              enabled: true,
+              sticky: false,
+            },
+            loop: false,
+            on: {
+              init: (swiper: any) => {
+                this.updateShadowsOutsideZone(swiper, index);
+              },
+              progress: (swiper: any) => {
+                this.updateShadowsOutsideZone(swiper, index);
+              }
+            }
+          });
         });
-      });
-    }, 100);  
+      }, 150);
+    });
   }
-
-
+  
+  updateShadowsOutsideZone(swiper: any, index: number) {
+    const showLeft = !swiper.isBeginning;
+    const showRight = !swiper.isEnd;
+    
+    // Only update if values actually changed
+    if (this.showLeftShadow[index] !== showLeft || this.showRightShadow[index] !== showRight) {
+      this.ngZone.run(() => {
+        this.showLeftShadow[index] = showLeft;
+        this.showRightShadow[index] = showRight;
+      });
+    }
+  }
+  
   ngOnInit(): void {
     this.dataService.getProjects().subscribe(data => {
       this.projects = data.projects;
+
+      this.projects.forEach((_, index) => {
+        this.showLeftShadow[index] = false;
+        this.showRightShadow[index] = true;
+      });
     });
   }
 }
