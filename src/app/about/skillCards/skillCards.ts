4b35936b07815e9ef2ca5736950598f262f75c9b
Tthide <bordethibault@gmail.com>
2025-11-28 19:21:07 +0100

    feat: added collapsable skill cards

--- src/app/about/skillCards/skillCards.ts
+++ src/app/about/skillCards/skillCards.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 
-
 @Component({
-    selector: 'skill-card',
-    imports: [],
-    templateUrl: './skillCards.html',
-    styleUrl: '../about.css',
+  selector: 'skill-card',
+  templateUrl: './skillCards.html',
+  styleUrls: ['../about.css'],
   standalone: true,
 })
 export class SkillCardsComponent {
+  // Track open/closed state of each card
+  isOpen: { [key: number]: boolean } = {};
 
+  toggle(id: number) {
+    this.isOpen[id] = !this.isOpen[id];
+  }
 }
