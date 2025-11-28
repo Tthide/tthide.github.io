d6182ba582bd6438e78a3d4a45845d1dc8044fd9
Tthide <bordethibault@gmail.com>
2025-11-28 18:21:19 +0100

    refactor: created skill cards component

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ContactFormComponent } from '../contact/contactForm/contactForm';
 import { RotatingTextIconComponent } from '../shared/rotating-text-icon/rotating-text-icon';
+import { SkillCardsComponent } from './skillCards/skillCards';
 
 @Component({
   selector: 'app-about',
-  imports: [ContactFormComponent, RotatingTextIconComponent],
+  imports: [ContactFormComponent, RotatingTextIconComponent,SkillCardsComponent],
   templateUrl: './about.html',
   standalone: true,
   styleUrl: './about.css'
---
+++ src/app/about/skillCards/skillCards.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'skill-card',
+    imports: [],
+    templateUrl: './skillCards.html',
+    styleUrl: '../about.css',
+  standalone: true,
+})
+export class SkillCardsComponent {
+
+}
